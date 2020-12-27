a75fc57be7d7f7534624bd205aede9876fd25a92
godot63 <lcalmbach@gmail.com>
2020-12-27 09:54:40 +0100

    initial

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-st.write('hello')
-f = open("demofile.txt", "w")
-f.write("Now the file has some content!")
-f.close()
+def main():
+    st.write('hello')
+    f = open("demofile.txt", "w")
+    f.write("Now the file has some content!")
+    f.close()
 
-f = open("demofile.txt", "r")
-st.write(f.read())
+    f = open("demofile.txt", "r")
+    st.write(f.read())
+
+if __name__ == '__main__':
+    main()
