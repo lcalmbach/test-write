cc3d699fc8226885a88b4755f1c17658bd86ae75
godot63 <lcalmbach@gmail.com>
2020-12-28 13:32:00 +0100

    added tmp folder

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 def main():
     st.write('hello')
-    f = open("demofile.txt", "w")
+    f = open("./tmp/demofile.txt", "w")
     f.write("Now the file has some content!")
     f.close()
 
-    f = open("demofile.txt", "r")
+    f = open("./tmp/demofile.txt", "r")
     st.write(f.read())
 
 if __name__ == '__main__':
