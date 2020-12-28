51478d5b54a69acba342200ad82fe6b94b3ae697
godot63 <lcalmbach@gmail.com>
2020-12-28 14:40:12 +0100

    introduced os.path.join

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
-
+import os
 def main():
     st.write('hello')
-    f = open("/tmp/demofile.txt", "w")
+
+    file_name = os.path.join("tmp", "demofile.txt")
+    f = open(file_name, "w")
     f.write("Now the file has some content!")
     f.close()
 
-    f = open("/tmp/demofile.txt", "r")
+    f = open(file_name, "r")
     st.write(f.read())
 
 if __name__ == '__main__':
