7c733573b066ee96b7c90d38ce65a327b9b5c73f
godot63 <lcalmbach@gmail.com>
2020-12-29 09:45:19 +0100

    used json format for test

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 import streamlit as st
 import os
+import json
+
+
+
+
+file_name = os.path.join("tmp", "scenarios.json")
+    
+def read_file():
+    with open(file_name, 'r') as myfile:
+        data=json.loads(myfile.read())
+        st.write("after read")
+        st.write(data)
+    return data
+    
+def change_file(data):
+    scenario = data["default"]
+    scenario["num_people"] = 1
+    st.write("after change")
+    st.write(data)
+    return data
+
+def write_file(data):
+    with open(file_name, 'w') as myfile:
+        json.dump(data, myfile)
+
 def main():
-    st.write('hello')
-
-    file_name = os.path.join("tmp", "demofile.txt")
-    f = open(file_name, "w")
-    f.write("Now the file has some content!")
-    f.close()
-
-    f = open(file_name, "r")
-    st.write(f.read())
-
+    st.title("test write on share.streamlit.io")
+    data = read_file()
+    data = change_file(data)
+    write_file(data)
+    data = read_file()
+    
 if __name__ == '__main__':
     main()
