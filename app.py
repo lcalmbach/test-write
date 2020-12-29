dea57dec83264ae7829f0ed8ce3d7b38d1060cc8
godot63 <lcalmbach@gmail.com>
2020-12-29 13:26:38 +0100

    write to different file

--- app.py
+++ app.py
@@ -2,35 +2,33 @@ import streamlit as st
 import os
 import json
 
-
-
-
 file_name = os.path.join("tmp", "scenarios.json")
+file_name2 = os.path.join("tmp", "scenarios2.json")
     
-def read_file():
-    with open(file_name, 'r') as myfile:
+def read_file(f):
+    with open(f, 'r') as myfile:
         data=json.loads(myfile.read())
-        st.write("after read")
-        st.write(data)
     return data
     
-def change_file(data):
+def change_content(data):
     scenario = data["default"]
     scenario["num_people"] = 1
-    st.write("after change")
-    st.write(data)
     return data
 
-def write_file(data):
-    with open(file_name, 'w') as myfile:
+def write_file(data, f):
+    with open(f, 'w') as myfile:
         json.dump(data, myfile)
 
 def main():
     st.title("test write on share.streamlit.io")
-    data = read_file()
-    data = change_file(data)
-    write_file(data)
-    data = read_file()
+    data = read_file(file_name)
+    st.write("after read")
+    st.write(data)
+    data = change_content(data)
+    write_file(data, file_name2)
+    data = read_file(file_name2)
+    st.write("after write")
+    st.write(data)
     
 if __name__ == '__main__':
     main()
